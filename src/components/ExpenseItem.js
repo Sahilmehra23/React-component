57b871028154be6b74f42b77147c8f6419925560
Sahilmehra23 <sahil@techis.io>
2022-11-11 23:46:19 +0530

    added

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+    return (
+        <div className='expense-item'>
+            <div>March 12th 2021</div>
+            <div className='ex'>
+         <h2>car insurance</h2>
+         <div className='expense-item_price'>200</div>
+         </div>
+         </div>
+
+
+    );
+
+}
+
+export default ExpenseItem;
