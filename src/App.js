3a1be10fc038c3aa26b2d8c7f5cea4c4fe85126a
Sahilmehra23 <sahil@techis.io>
2022-11-20 15:25:17 +0530

    new component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App(){
+  let expenses=[
+    {
+      id: 'e1',
+      title: 'school Fee',
+      amount: 250,
+      date: new Date(2021, 5, 12)
+    },
+    {
+      id: 'e2',
+      title: 'school Fee',
+      amount: 250,
+      date: new Date(2021, 5, 12)
+    },
+    {
+      id: 'e3',
+      title: 'school Fee',
+      amount: 250,
+      date: new Date(2021, 5, 12)
+    },
+  ]
   return (
-      <div>
-        <h2>lets go</h2>
-        <ExpenseItem></ExpenseItem>
-        </div>
+    <div>
+      <h2>lets go</h2>
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}>
+      </ExpenseItem>
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}>
+      </ExpenseItem>
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        amount={expenses[2].amount}>
+      </ExpenseItem>
+      
+    </div>
 
   )
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,27 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props){
+    const month = props.date.toLocaleString('en-US', {month: 'long' });
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString('en-US', {day: '2-digit' });
     return (
         <div className='expense-item'>
-            <div>March 12th 2021</div>
+            <div>
+                 
+                <div>{year}</div>
+                <div>{day}</div> 
+                <div>{month}</div>
+            </div>
             <div className='ex'>
-         <h2>car insurance</h2>
-         <div className='expense-item_price'>200</div>
-         </div>
-         </div>
+                <h2>{props.title}</h2>
+
+                <div className='expense-item_price'>${props.amount}</div>
+            </div>
+        </div>
+
 
 
     );
 
 }
-
 export default ExpenseItem;
