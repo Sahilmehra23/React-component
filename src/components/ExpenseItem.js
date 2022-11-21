df0630c701b27663abd408263037514495d2a6c7
Sahilmehra23 <sahil@techis.io>
2022-11-21 16:06:18 +0530

    added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props){
-    const month = props.date.toLocaleString('en-US', {month: 'long' });
-    const year = props.date.getFullYear();
-    const day = props.date.toLocaleString('en-US', {day: '2-digit' });
+    // const month = props.date.toLocaleString('en-US', {month: 'long' });
+    // const year = props.date.getFullYear();
+    // const day = props.date.toLocaleString('en-US', {day: '2-digit' });
     return (
         <div className='expense-item'>
-            <div>
-                 
-                <div>{year}</div>
-                <div>{day}</div> 
-                <div>{month}</div>
-            </div>
+            <ExpenseDate date ={props.date}/>
             <div className='ex'>
                 <h2>{props.title}</h2>
 
